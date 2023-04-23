9d36b9cbe158990cb8ab9abb05e747df7f56e225
giocar-sf <131580966+giocar-sf@users.noreply.github.com>
2023-04-23 19:40:00 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,6 +43,7 @@ streamlit.write('The user entered ', fruit_choice)
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice )
 
+streamlit.stop()
 import snowflake.connector
 
 
